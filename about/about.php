6421556187f6ac383ae388ee4f4dcfeb49c1f83e
duskdesign <dusk.design@neardusk.com>
2015-06-21 21:41:39 +0100

    first commit // PF

---
+++ about/about.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<title>Izzy Pink Cake Artistry</title>
+<link rel="stylesheet" href="/css/style_2.css">
+<link href="http://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,700,300italic,400italic,500italic,700italic|Varela+Round|Raleway:300,regular,500,600,700" id="PowerSlider1-webfonts" rel="stylesheet" type="text/css">
+<link href="../includes/jQuerySlider2/owl.transitions.css" rel="stylesheet" type="text/css">
+<link href="../includes/jQuerySlider2/PowerSlider1.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="../includes/jQuerySlider2/slider.extend.js"></script>
+<script type="text/javascript" src="../includes/jQuerySlider2/browser-compatibility.js"></script>
+<script type="text/javascript" src="../includes/jQuerySlider2/JQuerySlider2.js"></script>
+<script type="text/javascript" src="../includes/jQuerySlider2/PowerSlider1.js"></script>
+<link href="http://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,700,300italic,400italic,500italic,700italic|Varela+Round|Raleway:300,regular,500,600,700" id="PowerSlider2-webfonts" rel="stylesheet" type="text/css">
+<link href="../includes/jQuerySlider2/PowerSlider2.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="../includes/jQuerySlider2/slider.extend.js"></script>
+<script type="text/javascript" src="../includes/jQuerySlider2/browser-compatibility.js"></script>
+<script type="text/javascript" src="../includes/jQuerySlider2/JQuerySlider2.js"></script>
+<script type="text/javascript" src="../includes/jQuerySlider2/PowerSlider2.js"></script>
+</head>
+
+<body>
+
+<div id="header-wrapper">
+    <div class="header-container">
+        <div class="header">
+            <img src="/img/ipcake_logo_mk5.png" style='width:100%;' border="0" alt="Null">
+
+        </div>
+    </div>
+</div>
+
+<div id="nav-wrapper">
+    <div class="nav-menue">
+
+        <div class="nav-1"><a class="men-style" href="../index.php">HOME</a></div>
+
+        <div class="nav-1"><a class="men-style" href="#">ABOUT</a></div>
+
+        <div class="nav-1"><a class="men-style" href="#">GALLERY</a></div>
+
+        <div class="nav-1"><a class="men-style" href="#">EVENTS</a></div>
+
+        <div class="nav-1"><a class="men-style" href="#">CONTACT</a></div>
+
+        <select id="selectbox" name="" onchange="javascript:location.href = this.value;">
+            <option value="/index.php" >HOME</option>
+            <option selected="" >ABOUT</option>
+            <option href="#" >GALLERY</option>
+            <option href="#" >EVENTS</option>
+            <option href="#" >CONTACT</option>
+        </select>
+    </div>
+
+
+</div>
+
+<div id="body-wrapper">
+    <div class="body-container">
+        <div class="body-box">
+
+            <div class="pic-box-1">
+
+                <img src="/img/Little-Baker.png" style='width:100%;' border="0" alt="Null">
+            </div>
+
+            <div class="text-box-1">
+
+                <p style="margin-top:0; width: 100%;">
+                    As you can see, Izzy has been baking cakes from a young age although we aren’t sure how much mixture actually made it into the cake tin back then! Having shown a flair for art and design throughout her education, Izzy wanted to combine her two passions and start a career in the baking industry.
+                </p>
+
+                <p>
+                    After gaining a distinction for a City and Guilds qualification in cake decorating, Izzy honed her skills by working alongside some of London’s top cake decorators and freelancing for several of the capital’s premier baking and decorating companies as a tutor and cake artist. During this time, Izzy could be seen on Create and Craft TV, featured in Bake and Creativity magazines and worked on cakes for many celebrity clients.
+                </p>
+
+            </div>
+
+            <div class="clear"></div>
+
+            <div class="text-box-2">
+
+                <p style="margin-top:0;">
+                    After learning all she could about high end wedding and celebration cakes, Izzy started her own business, Izzy Pink Cake Artistry to be able to create her own cakes to her unique style. Since then Izzy has worked with large corporate clients such as J. Crew on their ‘Save the Bees’ campaign and her cookies were distributed in their Regent Street store and seen in Harrods. Izzy has also been featured on Rock My Wedding, one of the UKs most respected wedding blogs.
+
+                </p>
+
+                <p>
+                    Izzy is extremely passionate about creating bespoke designs for her clients so they can have a cake that not only tastes exceptional but is unique to them and their event. Clients can be as involved in the design process as they like or give Izzy a brief and let her do what she does best –  designing and making incredible cakes and cookies for all occasions.
+                </p>
+
+                <p>
+                    Izzy is based in Hampshire but can deliver to anywhere in the south of England, including London.
+                </p>
+            </div>
+
+            <div class="test-box">
+            <div id="PowerSlider2Container" class="">
+                <div id="PowerSlider2" class="PowerSlider2">
+                    <ul id="extend-slider" class="owl-carousel">
+                        <li class="slide1" title="slide1">
+
+
+
+                        <img class="customImage1" src="../includes/jQuerySlider2/PowerSlider2/test-1.png"></li>
+
+                        <li class="slide2" title="slide2">
+
+
+
+                        <img class="customImage2" src="../includes/jQuerySlider2/PowerSlider2/test-2.png"></li>
+                    </ul>
+
+		
+
+		
+
+                </div>
+            </div>
+
+            	
+
+            </div>
+
+            <div class="pic-box-2">
+
+                <img src="/img/Little-Baker.png" style='width:100%;' border="0" alt="Null">
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<title>Izzy Pink Cake Artistry</title>
+<link rel="stylesheet" href="/css/style_2.css">
+<link href="http://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,700,300italic,400italic,500italic,700italic|Varela+Round|Raleway:300,regular,500,600,700" id="PowerSlider1-webfonts" rel="stylesheet" type="text/css">
+<link href="includes/jQuerySlider2/owl.transitions.css" rel="stylesheet" type="text/css">
+<link href="includes/jQuerySlider2/PowerSlider1.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="includes/jQuerySlider2/slider.extend.js"></script>
+<script type="text/javascript" src="includes/jQuerySlider2/browser-compatibility.js"></script>
+<script type="text/javascript" src="includes/jQuerySlider2/JQuerySlider2.js"></script>
+<script type="text/javascript" src="includes/jQuerySlider2/PowerSlider1.js"></script>
+</head>
+
+<body>
+
+<div id="header-wrapper">
+    <div class="header-container">
+        <div class="header">
+            <img src="/img/ipcake_logo_mk5.png" style='width:100%;' border="0" alt="Null">
+        
+        </div>
+    </div>
+</div>
+
+<div id="nav-wrapper">
+    <div class="nav-menue">
+
+        <div class="nav-1"><a class="men-style" href="#">HOME</a></div>
+
+        <div class="nav-1"><a class="men-style" href="/about/about.php">ABOUT</a></div>
+
+        <div class="nav-1"><a class="men-style" href="#">GALLERY</a></div>
+
+        <div class="nav-1"><a class="men-style" href="#">EVENTS</a></div>
+
+        <div class="nav-1"><a class="men-style" href="#">CONTACT</a></div>
+
+        <select id="selectbox" name="" onchange="javascript:location.href = this.value;">
+            <option href="#" selected="">HOME</option>
+            <option value="/about/about.php" >ABOUT</option>
+            <option href="#" >GALLERY</option>
+            <option href="#" >EVENTS</option>
+            <option href="#" >CONTACT</option>
+        </select>
+
+    </div>
+
+
+</div>
+
+<div id="body-wrapper">
+    <div class="body-container">
+        <div class="body-box">
+<div id="PowerSlider1Container" class="">
+	<div id="PowerSlider1" class="PowerSlider2">
+		<ul id="extend-slider" class="owl-carousel">
+			<li class="slide1" title="slide1">
+				
+				
+				
+			</li>
+	  
+			<li class="slide2" title="slide2">		
+				
+				
+				
+			</li>
+		<li class="slide3" title="slide3">
+				
+				
+				
+		  </li><li class="slide5" title="slide5">		
+				
+				
+				
+			</li><li class="slide6" title="slide6">		
+				
+				
+				
+			</li></ul>
+
+		
+
+		<div id="PowerSlider1_back_in" class=""></div>
+		<div id="PowerSlider1_next_in" class=""></div>
+	</div>
+</div>
+
+        
+        	
+        
+        	
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+</body>
+
+
+
+
+</html>
